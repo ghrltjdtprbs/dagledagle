6d317d6faed3acf04c7c2bc06eee9b38a7276626
hym <ghrltjdtprbs@gmail.com>
2025-05-03 17:53:34 +0900

    feat : JWT strategy 및 인증 guard 추가

---
+++ src/common/strategy/jwt.strategy.ts
@@ -0,0 +1,19 @@
+// src/common/strategy/jwt.strategy.ts
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_ACCESS_SECRET!,
+    });
+  }
+
+  validate(payload: any) {
+    return { id: payload.sub, email: payload.email }; // req.user에 들어감
+  }
+}
