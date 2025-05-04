f8c1d64451b7e12b8c96cb906c81c1be240f90b1
hym <ghrltjdtprbs@gmail.com>
2025-05-05 00:51:31 +0900

    feat : 좋아요시 게시글 작성자에게 알람 전송

--- src/domain/like/command/like.command.service.ts
+++ src/domain/like/command/like.command.service.ts
@@ -4,21 +4,33 @@ import { Repository } from 'typeorm';
 import { LikeEntity } from '../entity/like.entity';
 import { PostEntity } from '../../post/entity/post.entity';
 import { UserEntity } from '../../user/entity/user.entity';
+import { EventEmitter2 } from '@nestjs/event-emitter';
+import { NotificationEvent } from '../../notification/event/notification.event';
 
 @Injectable()
 export class LikeCommandService {
   constructor(
     @InjectRepository(LikeEntity)
     private readonly likeRepo: Repository<LikeEntity>,
+
     @InjectRepository(PostEntity)
     private readonly postRepo: Repository<PostEntity>,
+
     @InjectRepository(UserEntity)
     private readonly userRepo: Repository<UserEntity>,
+
+    private readonly eventEmitter: EventEmitter2,
   ) {}
 
   async toggle(postId: number, userId: number): Promise<boolean> {
-    const post = await this.postRepo.findOneByOrFail({ id: postId });
+    const post = await this.postRepo.findOne({
+      where: { id: postId },
+      relations: ['author'],
+    });
+
     const user = await this.userRepo.findOneByOrFail({ id: userId });
+
+    if (!post) throw new Error('게시글이 존재하지 않습니다.');
 
     const existing = await this.likeRepo.findOne({
       where: { post: { id: postId }, user: { id: userId } },
@@ -35,6 +47,17 @@ export class LikeCommandService {
     await this.likeRepo.save(like);
     post.likeCount++;
     await this.postRepo.save(post);
+
+    if (post.author.id !== userId) {
+      this.eventEmitter.emit(
+        'notification.created',
+        new NotificationEvent(
+          post.author.id,
+          `게시글 "${post.title}"에 좋아요가 눌렸습니다.`,
+        ),
+      );
+    }
+
     return true;
   }
 }
