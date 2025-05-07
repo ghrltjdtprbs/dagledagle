34001f541d1654d9234fdedb89c1533d2175104f
hym <ghrltjdtprbs@gmail.com>
2025-05-07 22:28:17 +0900

    fix : RDS SSL 인증서 검증 생략

--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -1,13 +1,15 @@
 // src/config/config.prod.ts
 export default () => ({
-    database: {
-      host: process.env.PROD_DB_HOST,
-      port: +(process.env.PROD_DB_PORT ?? '5432'),
-      username: process.env.PROD_DB_USERNAME,
-      password: process.env.PROD_DB_PASSWORD,
-      name: process.env.PROD_DB_DATABASE,
-      logging: false,
+  database: {
+    host: process.env.PROD_DB_HOST,
+    port: +(process.env.PROD_DB_PORT ?? '5432'),
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    name: process.env.PROD_DB_DATABASE,
+    logging: false,
+    ssl: {
+      rejectUnauthorized: false, 
     },
-    swagger: false,
-  });
-  
+  },
+  swagger: false,
+});
